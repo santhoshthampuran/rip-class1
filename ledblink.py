209e3d37f0f59d21534d6077bcd0c73d659a034f
Santhosh Thampuran <santhosh.thampuran@gmail.com>
2017-10-22 09:21:20 -0400

    refactored code

--- ledblink.py
+++ ledblink.py
@@ -1,15 +1,18 @@
 from gpiozero import LED
 from time import sleep
 
-led = LED(17)
-print("setting to GPIO 17...")
 
-count = 0
+# blinking function
+def blink(pin, times=None):
+	led = LED(pin)
+	print("setting to GPIO 17...")
+	print (times)
+	for i in range(0, times):
+		led.on()
+		sleep(1)
+		led.off()
+		sleep(1)
+		print("cycle {0}".format(i))
 
-while True:
-    led.on()
-    sleep(1)
-    led.off()
-    sleep(1)
-    print("cycle {0}".format(count))
-    count += 1
+
+blink(17, 5)
